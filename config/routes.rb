02a67575275d5b43d861c127628cb3ccb87a5bc2
suzuki <s.suzukitime0211@gmail.com>
2021-07-09 13:20:53 +0000

    update7/9

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    devise_for :admins, controllers: {
+      sessions:      'admin/admins/sessions',
+      passwords:     'admin/admins/passwords',
+      registrations: 'admin/admins/registrations'
+    }
+  end
+
+  namespace :user do
+    devise_for :users, controllers: {
+      sessions: 'user/users/sessions',
+      registrations: 'user/users/registrations',
+      passwords: 'user/users/passwords'
+    }
+  end
+  root to: 'homes#top'
+end
